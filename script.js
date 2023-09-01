9ea7239650f19baa00befe235f725130400cc50b
tomas <tomas153lm@gmail.com>
2023-09-01 10:07:15 -0300

    feat: error message

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const language = 'en';
 const getTemp = async () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById("search-input").value}&lang=${language}&appid=${key}&units=metric`
 
-    const query = await fetch(url, {
-        method: 'GET'
-    });
+    
 
-    const request = await query.json();
+    document.querySelector(".temp-container").style.display = 'flex';
+    document.querySelector('.container').style.justifyContent = 'space-evenly'
+    document.getElementById("image-temp").style.display = 'block';
 
-    console.log(request);
+    try {
+        const query = await fetch(url, {
+            method: 'GET'
+        });
 
-    if(request.cod !== 404 && request.cod !== 400) {
+        
+    
+        const request = await query.json();
+
+        if(!query.ok) {
+            throw new Error(request.message);
+        }
+    
+        console.log(request);
+    
         const isDay = request.weather[0].icon.indexOf("d") === -1 ? false : true;
-
-        document.querySelector(".temp-container").style.display = 'flex';
-        document.querySelector('.container').style.justifyContent = 'space-evenly'
-        document.getElementById("image-temp").style.display = 'block';
 
         switch(request.weather[0].main) {
             case 'Clear':
@@ -63,17 +71,28 @@ const getTemp = async () => {
             break;
         }
 
+        document.getElementById("name").innerHTML = request.name;
         document.getElementById("temp-text").innerHTML = parseInt(request.main.temp) + '<span class="metric-text">°C</span>';
         document.getElementById("min").innerHTML = 'Minimum: ' + parseInt(request.main.temp_min) + '°C';
         document.getElementById("max").innerHTML = 'Maximum: ' + parseInt(request.main.temp_max) + '°C';
         document.getElementById("pressure").innerHTML = 'Pressure: ' + parseInt(request.main.pressure) + 'hpa';
         document.getElementById("humidity").innerHTML = 'Humidity: ' + parseInt(request.main.humidity) + '%';
+    } catch(e) {
+        console.log(e);
+        document.getElementById("image-temp").src = 'src/svg/cross-icon.svg';
+        document.getElementById("name").innerHTML = '';
+        const message = e.message.charAt(0).toUpperCase() + e.message.slice(1);
+        
+        document.getElementById("temp-text").innerHTML = message;
+        document.getElementById("min").innerHTML = '';
+        document.getElementById("max").innerHTML = '';
+        document.getElementById("pressure").innerHTML = '';
+        document.getElementById("humidity").innerHTML = '';
     }
     // if(request.weather[0].description === 'clear sky') {
     //     
     //     document.getElementById("image-temp").src = 'https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/2nd%20Set%20-%20Color/clear-day.png'
     // }
-    console.log(request);
 }
 
 // https://basmilius.github.io/weather-icons/index-fill.html
